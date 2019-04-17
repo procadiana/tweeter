028375a2126fbf8384d49cc436fa4e508662a108
procadiana <diana.claudia.ilinca@gmail.com>
2019-04-17 23:38:11 +0000

    Form Validation

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,18 +1,26 @@
 $(document).ready(function() {
   let elem = document.getElementById('tweet');
+  let button = document.getElementById('button');
   elem.addEventListener('keyup', countCharacters, false);
+
+  button.addEventListener('click', function(){
+    let value = document.getElementById('tweet').value;
+    if (value.length === 0){
+      alert("Please enter a tweet");
+    }
+  })
+
   function countCharacters(event) {
-    input = document.getElementById('tweet').value;
-    counter = (140 - (input.length));
-    countRemaining = document.getElementById('counter');
+    let input = document.getElementById('tweet').value;
+    let counter = (140 - (input.length));
+    let countRemaining = document.getElementById('counter');
     if (counter >= 0){
       countRemaining.textContent = counter;
     }
     else {
       countRemaining.textContent = counter;
       countRemaining.style.color = 'red';
+      alert("Tweet too long!!!");
     }
-
   };
-
 });
